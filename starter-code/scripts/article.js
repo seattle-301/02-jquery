305e32fc8c25424e7504f3aac31aa701d9221f99
Rick Patci <rpatci@gmail.com>
2016-06-28 14:55:23 -0700

    add solution lab-02

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,16 +1,22 @@
 var articles = [];
 
 function Article (opts) {
-  // TODO: Use the object passed in to complete this constructor function:
+  // TODO: Use the object passed in to complete this contructor function:
   // Save ALL the properties of `opts` into `this`.
   this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.title = opts.title;
+  this.category = opts.category;
+  this.body = opts.body;
+  this.publishedOn = opts.publishedOn;
 }
 
 // Define a method on the Article prototype
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
-
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   /* TODO: Now use jQuery to fill in the rest of the current
   template clone with properties from this particular Article instance.
   We need to fill in:
@@ -18,16 +24,18 @@ Article.prototype.toHtml = function() {
    2. author url,
    3. article title,
    4. article body, and
-   5. publication date. */
-
-  // Display the date as a relative number of "days ago":
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-  $newArticle.find('time').html('about' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-
+   5. publication date as a datetime attribute. */
+  $newArticle.attr('data-author', this.author);
+  $newArticle.find('.byline a').html(this.author);
+  $newArticle.find('.byline a').attr('href', this.authorUrl);
+  $newArticle.find('h1:first').html(this.title);
+  $newArticle.find('.article-body').html(this.body);
+  $newArticle.find('time[pubdate]').attr('datetime', this.publishedOn);
   /* TODO: This cloned article is no longer a template,
    as it now has real data attached to it! We need to account
    for that and change it before this current article gets
    rendered to our DOM. */
+  $newArticle.removeClass('template');
   return $newArticle;
 };
 // Sort our data by date published, descending order
@@ -39,9 +47,8 @@ ourLocalData.sort(function(a,b) {
 ourLocalData.forEach(function(ele) {
   articles.push(new Article(ele));
 });
-
 /* Append each Article to the DOM.
 NOTE: Remember that the '.toHtml' method invoked is one WE created. */
-articles.forEach(function(article) {
-  $('#articles').append(article.toHtml());
+articles.forEach(function(a){
+  $('#articles').append(a.toHtml());
 });
