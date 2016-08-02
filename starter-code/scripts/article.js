0146f426a8f46629868c4b7e9446af96823bf038
Rick Patci <rpatci@gmail.com>
2016-08-02 15:37:52 -0700

    change method .html to .text

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -16,7 +16,7 @@ Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
   $newArticle.attr('data-category', this.category);
   $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.find('time').text('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   /* TODO: Now use jQuery to fill in the rest of the current
   template clone with properties from this particular Article instance.
   We need to fill in:
